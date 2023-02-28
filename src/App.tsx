4c584f753ba62195c38f01a0e27d291c47b7a7dc
Rafael Treiber <rafae.g.treiber@exxonmobil.com>
2023-02-28 16:55:40 -0300

    adding themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { ThemeProvider } from 'styled-components';
 import { Main } from './components/Main';
+import { defaultTheme } from './styles/themes/default';
 
 function App() {
-  return <Main />;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Main />;
+    </ThemeProvider>
+  );
 }
 
 export default App;
